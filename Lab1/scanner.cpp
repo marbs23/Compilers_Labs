92d40df8b8d08419bc1cbbb13ac3efb8c4ce8f75
Martin Bonilla <martinbonillasarmiento@gmail.com>
2026-04-01 16:41:38 -0500

    feat: fix some bug about lexeme

--- Lab1/scanner.cpp
+++ Lab1/scanner.cpp
@@ -9,14 +9,16 @@ using namespace std;
 // -----------------------------
 // Constructor
 // -----------------------------
-Scanner::Scanner(const char* s): input(s), first(0), current(0) { 
-    }
+Scanner::Scanner(const char *s) : input(s), first(0), current(0), current_line(1), current_col(1)
+{
+}
 
 // -----------------------------
 // Función auxiliar
 // -----------------------------
 
-bool is_white_space(char c) {
+bool is_white_space(char c)
+{
     return c == ' ' || c == '\n' || c == '\r' || c == '\t';
 }
 
@@ -24,17 +26,16 @@ bool is_white_space(char c) {
 // nextToken: obtiene el siguiente token
 // -----------------------------
 
-
 /*
 Token* Scanner::nextToken() {
     Token* token;
 
     // Saltar espacios en blanco
-    while (current < input.length() && is_white_space(input[current])) 
+    while (current < input.length() && is_white_space(input[current]))
         current++;
 
     // Fin de la entrada
-    if (current >= input.length()) 
+    if (current >= input.length())
         return new Token(Token::END);
 
     char c = input[current];
@@ -79,143 +80,208 @@ Token* Scanner::nextToken() {
     return token;
 }
 */
- 
 
-Token* Scanner::nextToken() {
-    Token* token;
+Token *Scanner::nextToken()
+{
+    Token *token;
     char c;
     state = 0;
-    first = current;;
+    first = current;
+    ;
 
-    while (1) {
-        switch (state) {
-            case 0: 
-                c = nextChar();
-                if (is_white_space(c)) { first = current;; state = 0; }
-                else if (c == '\0') return new Token(Token::END);
-                else if (c == '(') state = 1;
-                else if (c == ')') state = 2;
-                else if (c == '+') state = 3;
-                else if (c == '-') state = 4;
-                else if (c == '*') state = 5;
-                else if (c == '/') state = 6;
-                else if (c == ';') state = 7;
-                else if (isdigit(c)) state = 8;
-                else if (isalpha(c)) state = 11;
-                else return new Token(Token::ERR, c);
-                break;
+    while (1)
+    {
+        switch (state)
+        {
+        case 0:
+            c = nextChar();
+            if (is_white_space(c))
+            {
+                first = current;
+                ;
+                state = 0;
+            }
+            else if (c == '\0')
+                return new Token(Token::END, current_line, current_col);
+            else if (c == '(')
+                state = 1;
+            else if (c == ')')
+                state = 2;
+            else if (c == '+')
+                state = 3;
+            else if (c == '-')
+                state = 4;
+            else if (c == '*')
+                state = 5;
+            else if (c == '/')
+                state = 6;
+            else if (c == ';')
+                state = 7;
+            else if (isdigit(c))
+                state = 8;
+            else if (isalpha(c))
+                state = 11;
+            else
+                return new Token(Token::ERR, c, current_line, current_col);
+            break;
 
-            case 1: return new Token(Token::LPAREN);
-            case 2: return new Token(Token::RPAREN);
-            case 3: return new Token(Token::PLUS, c);
-            case 4: return new Token(Token::MINUS, c);
-            case 5: {
-                c = nextChar();
-                if (c == '*') return new Token(Token::POW, "**");
-                else {
-                    rollBack();
-                    return new Token(Token::MUL, c);
-                }
+        case 1:
+            return new Token(Token::LPAREN, current_line, current_col);
+        case 2:
+            return new Token(Token::RPAREN, current_line, current_col);
+        case 3:
+            return new Token(Token::PLUS, c, current_line, current_col);
+        case 4:
+            return new Token(Token::MINUS, c, current_line, current_col);
+        case 5:
+        {
+            c = nextChar();
+            if (c == '*')
+                return new Token(Token::POW, "**", current_line, current_col);
+            else
+            {
+                rollBack();
+                return new Token(Token::MUL, c, current_line, current_col);
             }
-            case 7: return new Token(Token::SEMICOLON, c);
-            case 6: return new Token(Token::DIV, c);
-            case 8: 
-                c = nextChar();
-                if (isdigit(c)) state = 8;
-                else if (c == '.') state = 10;
-                else state = 9;
-                break;
-            case 9:
-                rollBack();
-                return new Token(Token::NUM, input, first, current - first);
-            case 10:
-                c = nextChar();
-                if (isdigit(c)) state = 10;
-                else state = 13;
-                break;
-            case 11:
-                c = nextChar();
-                if (isalpha(c) || isdigit(c)) state = 11;
-                else state = 12;
-                break;
-            case 12: {
-                rollBack();
-                string text_aux = input.substr(first, current - first);
-                Token::Type type = checkReserved(text_aux);
-                return new Token(type, input, first, current - first);
-            }
-            case 13:
-                rollBack();
-                return new Token(Token::FLOAT, input, first, current - first);
+        }
+        case 7:
+            return new Token(Token::SEMICOLON, c, current_line, current_col);
+        case 6:
+            return new Token(Token::DIV, c, current_line, current_col);
+        case 8:
+            c = nextChar();
+            if (isdigit(c))
+                state = 8;
+            else if (c == '.')
+                state = 10;
+            else
+                state = 9;
+            break;
+        case 9:
+            rollBack();
+            return new Token(Token::NUM, input, first, current - first, current_line, current_col);
+        case 10:
+            c = nextChar();
+            if (isdigit(c))
+                state = 10;
+            else
+                state = 13;
+            break;
+        case 11:
+            c = nextChar();
+            if (isalpha(c) || isdigit(c))
+                state = 11;
+            else
+                state = 12;
+            break;
+        case 12:
+        {
+            rollBack();
+            string text_aux = input.substr(first, current - first);
+            Token::Type type = checkReserved(text_aux);
+            return new Token(type, input, first, current - first, current_line, current_col);
+        }
+        case 13:
+            rollBack();
+            return new Token(Token::FLOAT, input, first, current - first, current_line, current_col);
         }
     }
 }
 
-
-Token::Type Scanner::checkReserved(const string& text) {
+Token::Type Scanner::checkReserved(const string &text)
+{
     if (text == "SIN" || text == "COS" || text == "TAN" ||
-        text == "LOG" || text == "SQRT") return Token::FUNC;
-    else if (text == "PI" || text == "E") return Token::CONST;
-    else return Token::ID;
+        text == "LOG" || text == "SQRT")
+        return Token::FUNC;
+    else if (text == "PI" || text == "E")
+        return Token::CONST;
+    else
+        return Token::ID;
 }
 
-void Scanner::rollBack() {
+void Scanner::rollBack()
+{
     if (current > 0)
+    {
         current--;
+        current_col--;
+        if (input[current] == '\n')
+            current_line--;
+    }
 }
 
-char Scanner::nextChar() {
+char Scanner::nextChar()
+{
     int c = input[current];
-    if (c != '\0') current++;
+    if (c != '\0')
+    {
+        current++;
+        if (c == '\n')
+        {
+            current_line++;
+            current_col = 0;
+        }
+    } else {
+        return c;
+    }
+    current_col++;
     return c;
 }
-
-
 
 // -----------------------------
 // Destructor
 // -----------------------------
-Scanner::~Scanner() { }
+Scanner::~Scanner() {}
 
 // -----------------------------
 // Función de prueba
 // -----------------------------
 
-void ejecutar_scanner(Scanner* scanner, const string& InputFile) {
-    Token* tok;
+void ejecutar_scanner(Scanner *scanner, const string &InputFile)
+{
+    Token *tok;
 
     // Crear nombre para archivo de salida
     string OutputFileName = InputFile;
     size_t pos = OutputFileName.find_last_of(".");
-    if (pos != string::npos) {
+    if (pos != string::npos)
+    {
         OutputFileName = OutputFileName.substr(0, pos);
     }
     OutputFileName += "_tokens.txt";
 
     ofstream outFile(OutputFileName);
-    if (!outFile.is_open()) {
+    if (!outFile.is_open())
+    {
         cerr << "Error: no se pudo abrir el archivo " << OutputFileName << endl;
         return;
     }
 
-    outFile << "Iniciando Scanner para archivo: " << InputFile << endl << endl;
+    outFile << "Iniciando Scanner para archivo: " << InputFile << endl
+            << endl;
 
-    while (true) {
+    while (true)
+    {
         tok = scanner->nextToken();
 
-        if (tok->type == Token::END) {
+        if (tok->type == Token::END)
+        {
             outFile << *tok << endl;
             delete tok;
-            outFile << "\nScanner exitoso" << endl << endl;
+            outFile << "\nScanner exitoso" << endl
+                    << endl;
             outFile.close();
             return;
         }
 
-        if (tok->type == Token::ERR) {
+        if (tok->type == Token::ERR)
+        {
             outFile << *tok << endl;
             delete tok;
-            outFile << "Caracter invalido" << endl << endl;
-            outFile << "Scanner no exitoso" << endl << endl;
+            outFile << "Caracter invalido" << endl
+                    << endl;
+            outFile << "Scanner no exitoso" << endl
+                    << endl;
             outFile.close();
             return;
         }
